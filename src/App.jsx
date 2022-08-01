5526beda2a82c86b7a1a4c874d86f657c0cf1663
Joel Trinidad <jth_2929@hotmail.com>
2022-08-01 13:45:06 -0500

    container component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "semantic-ui-css/semantic.min.css";
-import { Button, Icon } from "semantic-ui-react";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <Button animated>
-      <Button.Content visible>Next</Button.Content>
-      <Button.Content hidden>
-        <Icon name="arrow right" />
-      </Button.Content>
-    </Button>
+    <Container />
   );
 }
 
