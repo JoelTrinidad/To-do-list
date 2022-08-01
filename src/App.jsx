17333b4fa2631f615eb0ff0404c69258f5b2a04b
Joel Trinidad <jth_2929@hotmail.com>
2022-08-01 14:46:15 -0500

    InputTask component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "semantic-ui-css/semantic.min.css";
 import Container from "./components/Container";
 import Header from "./components/Header";
-
+import InputTask from "./components/InputTask";
 function App() {
   return (
     <Container>
       <Header></Header>      
+      <InputTask></InputTask>
     </Container>
   );
 }
