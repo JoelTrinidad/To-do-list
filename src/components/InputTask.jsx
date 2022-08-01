bc7ecefb6b75b434958fa86002da3b4553bcc8b6
Joel Trinidad <jth_2929@hotmail.com>
2022-08-01 16:37:45 -0500

    set select values

--- src/components/InputTask.jsx
+++ src/components/InputTask.jsx
@@ -1,13 +1,25 @@
 import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
+import { useState } from "react";
 
 const options = [
-  { key: "deporte", text: "Deporte", value: "deporte" },
-  { key: "casa", text: "Casa", value: "casa" },
-  { key: "oficina", text: "Oficina", value: "oficina" },
-  { key: "otra", text: "Otra", value: "otra" },
+    { key: "deporte", text: "Deporte", value: "deporte" },
+    { key: "casa", text: "Casa", value: "casa" },
+    { key: "oficina", text: "Oficina", value: "oficina" },
+    { key: "otra", text: "Otra", value: "otra" },
 ];
+
 export default function InputTask() {
-  return (
+    const [task, setTask] = useState({
+        idTask: "",
+        taskName: "",
+        categoryName: ""
+    });
+    const [error, setError] = useState(false);
+    
+    const onChangeTask = (e) => {
+        console.log("escribiendo");
+    }
+    return (
     <>
       <Grid centered columns={2}>
         <Input type="text" action>
@@ -17,7 +29,9 @@ export default function InputTask() {
             placeholder="Escribe tu tarea..."
             iconPosition="left"
             name="taskName"
-          />
+            value={task.taskName}
+            onChange={onChangeTask}
+            />
         </Input>
         <Select
           compact
@@ -25,11 +39,20 @@ export default function InputTask() {
           className="select-form-task"
           name="categoryTask"
           placeholder="Categoria"
+          value={task.categoryName}
         />
         <Button type="submit" color="violet">
           Añadir tarea
         </Button>
       </Grid>
+      {error && (
+        <Grid centered>
+            <Header as="h4" color="red" className="alert-error-form">
+                <Icon name="close"/>
+                <Header.Content>La tarea es obligatoia</Header.Content>
+            </Header>
+        </Grid>
+      )}
     </>
   );
 }
